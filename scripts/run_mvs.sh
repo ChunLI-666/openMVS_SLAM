a2ab5006d7aa1a4d5e27a880023aa3b055fcabfa
charles.li2 <charles.li2@cn.bosch.com>
2024-03-11 15:35:17 +0800

    add MVS pipeline for smartscanner

---
+++ scripts/run_mvs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 设置工作目录
+WORK_DIR="/home/icl2sgh/catkin_fastlio_ws/src/FAST_LIO_SLAM/FAST-LIO/result/zhongnan/fastlio_test_0311"
+CMD_DIR="/home/icl2sgh/project/openMVS_build/bin"
+# 输入和输出文件
+INPUT_MVS_FRAME="$WORK_DIR/mvs_frame_result.txt"
+DENSIFY_OUTPUT="mvs_densify.mvs"
+RECONSTRUCT_OUTPUT="mvs_reconstruct.mvs"
+REFINE_OUTPUT="mvs_refine.mvs"
+TEXTURE_OUTPUT="mvs_texture.mvs"
+
+# DensifyPointCloud
+echo "Running DensifyPointCloud..."
+$CMD_DIR/DensifyPointCloud -w "$WORK_DIR" -i "$INPUT_MVS_FRAME" -o "$WORK_DIR/$DENSIFY_OUTPUT"
+
+# ReconstructMesh
+echo "Running ReconstructMesh..."
+$CMD_DIR/ReconstructMesh -w "$WORK_DIR" -i "$WORK_DIR/$DENSIFY_OUTPUT" -o "$WORK_DIR/$RECONSTRUCT_OUTPUT"
+
+# RefineMesh
+echo "Running RefineMesh..."
+$CMD_DIR/RefineMesh -w "$WORK_DIR" -i "$WORK_DIR/$RECONSTRUCT_OUTPUT" -o "$WORK_DIR/$REFINE_OUTPUT"
+
+# TextureMesh
+echo "Running TextureMesh..."
+$CMD_DIR/TextureMesh -w "$WORK_DIR" -i "$WORK_DIR/$REFINE_OUTPUT" -o "$WORK_DIR/$TEXTURE_OUTPUT"
+
+echo "All commands executed successfully."
